ccf2b37e127eafe631f8fc64f4c962790d618107
JalalToufik <112856590+JalalToufik@users.noreply.github.com>
2023-11-16 14:33:48 +0100

    basic setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+    import '../lib/style/global.css'
+    import '../lib/style/fonts.css'
+</script>
+
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Header from "../lib/components/Header.svelte";
+
+    // export let data
+
+</script>
+
+<svelte:head>
+    <title>INK</title>
+</svelte:head>
+
+<Header />
+
+<main>
+
+</main>
+
+
+<style>
+
+</style>
